91c70988f8480b5945f57e355f3b33a5a3a10801
Valysa <jules.belletre@gmail.com>
2024-03-20 11:44:42 +0100

    work on sending mails

--- contact.php
+++ contact.php
@@ -3,6 +3,51 @@
     <head>
         <title>Lafleur Enterprise</title>
         <link rel="stylesheet" type="text/css" href="contact.css">
+        <?php
+        use PHPMailer\PHPMailer\PHPMailer;
+        use PHPMailer\PHPMailer\Exception;
+        require 'PHPMailer/src/Exception.php';
+        require 'PHPMailer/src/PHPMailer.php';
+        require 'PHPMailer/src/SMTP.php';
+        //NIQUE TA MERE LA PUTE LA GRANDE PUTE PUTE (date)
+        $lname = $_POST["lname"];
+        $fname = $_POST["fname"];
+        $email = $_POST["email"];
+        $subject = $_POST["subject"];
+        //sexe le zizi le zizi
+        $message = $_POST["content"];
+        $subject = $_POST["subject"];
+        $email = $_POST["mailref"];
+
+        //generation du mail
+        $mail = new PHPMailer(true);
+        try { 
+            $mail->isSMTP();
+            $mail->Host = 'smtp.office365.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = 'engagementjeunes64@outlook.fr';
+            $mail->Password = 'EngJeunes64&';
+            $mail->SMTPSecure = 'tls';
+            $mail->Port = 587;
+
+        // Set other email parameters (to, subject, message, headers)
+        $mail->setFrom('engagementjeunes64@outlook.fr');
+
+        //destination
+        $mail->addAddress($email);
+        $mail->Subject = $subject;
+        $mail->Body = $message;
+
+        // Send the email
+        $mail->send();
+            echo 'Merci de votre retour, le mail a bien été envoyé'; 
+        }  catch (Exception $e) { 
+            echo 'Une erreur est survenue lors de l envoi de mail, veuillez reessayer' . $mail->ErrorInfo; 
+        }
+        // echo $message ;
+        //header("Location: validationcons.html");
+        exit;
+        ?>
     </head>
     <body>
         <header>
